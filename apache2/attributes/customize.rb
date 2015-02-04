a11b83ef3fcdd6b1f84e45f32fd5f956b3691cb4
cptphpo6 <raja.s@changepond.com>
2015-02-04 21:10:45 +0530

    image and sound path

---
+++ apache2/attributes/customize.rb
@@ -0,0 +1,4 @@
+#image in the assets folder
+default[:apache][:imgdir] = '/srv/www/immweb/current/imm-assets/img/'
+#Sound in the assets folder
+default[:apache][:sounddir] = '/srv/www/immweb/current/imm-assets/sound/'
