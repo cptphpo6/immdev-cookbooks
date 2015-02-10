2c2058248c1ee83ade8df089434d14270c908396
cptphpo6 <raja.s@changepond.com>
2015-02-10 11:38:13 +0530

    images and sounds url attributes

--- apache2/attributes/customize.rb
+++ apache2/attributes/customize.rb
@@ -2,3 +2,7 @@
 default[:apache][:imgdir] = '/var/www/img/'
 #Sound in the assets folder
 default[:apache][:sounddir] = '/var/www/sound/'
+#image in the assets folder
+default[:apache][:img_url] = '/var/www/img/'
+#Sound in the assets folder
+default[:apache][:sound_url] = '/var/www/sound/'
